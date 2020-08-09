7880923cc49128297cd73e5e198cc3c8dbb5c814
Ty Ebanks <ebankst07@gmail.com>
2020-08-09 22:37:30 +0100

    Comments added to gatsby.browser.js file

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,5 +3,5 @@
  *
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
-
-// You can delete this file if you're not using it
+// This file is for implementing any of the APIs available. This is done by exporting them from a file named gatsby-browser.js in the root of your project.
+// If this file is not used it can be deleted.
