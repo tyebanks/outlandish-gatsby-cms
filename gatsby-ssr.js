188263f4a25c21344bfaad648625a39e7d2c3980
Ty Ebanks <ebankst07@gmail.com>
2020-08-09 22:11:26 +0100

    Brief Description of gatsby-ssr.js file added

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,5 +3,5 @@
  *
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
+// The file gatsby-ssr.js lets you alter the content of static HTML files as they are being Server-Side Rendered (SSR) by Gatsby and Node.js.
 
-// You can delete this file if you're not using it
